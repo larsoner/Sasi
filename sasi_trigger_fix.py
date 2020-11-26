dd915e1bbf11da32e70160aa3fe307a4eedcd734
Maggie Clarke <mdclarke@uw.edu>
2020-11-26 11:18:50 -0800

    Update sasi_trigger_fix.py

--- sasi_trigger_fix.py
+++ sasi_trigger_fix.py
@@ -2,7 +2,6 @@
 """
 script to produce event list with correct triggers in sasi data, 
 to use for averaging. Adjust script for a or b and subject group
-
 """
 
 from process_sasi_list import parse_list
@@ -11,24 +10,24 @@ import mne
 import numpy as np
 import os.path as op
 
-
+list_type = 'B' # A or B
 data_path = '/storage/Maggie/sasi/'
-## A
-#list_fname = data_path + 'sentnew2a_FishNew.lst'
-## B
-list_fname = data_path + 'sentnew2b_FishNew.lst'
+
+if list_type == 'A':
+         list_fname = data_path + 'sentnew2a_FishNew.lst'
+         subjs = ['sasi_117', 'sasi_121', 'sasi_129', 'sasi_131', 
+                  'sasi_133', 'sasi_135', 'sasi_137', 'sasi_143',
+                  'sasi_145']
+if list_type == 'B':
+         list_fname = data_path + 'sentnew2b_FishNew.lst'
+         subjs = ['sasi_110', 'sasi_114', 'sasi_118', 'sasi_120',
+                  'sasi_130', 'sasi_134', 'sasi_141', 'sasi_144',
+                  'sasi_147']
+else:
+         print("list_type must be A or B")
 
 list_info = parse_list(list_fname)
 list_info_temp = list_info
-
-## A
-#subjs = ['sasi_117', 'sasi_121', 'sasi_129', 'sasi_131', 
-#         'sasi_133', 'sasi_135', 'sasi_137', 'sasi_143',
-#         'sasi_145']
-## B
-subjs = ['sasi_110', 'sasi_114', 'sasi_118', 'sasi_120',
-         'sasi_130', 'sasi_134', 'sasi_141', 'sasi_144',
-         'sasi_147']
 
 for i in subjs:
     raw_fname = op.join(data_path, '%s' %i, 'raw_fif', '%s_raw.fif' %i)
@@ -43,10 +42,9 @@ for i in subjs:
                         %i)
     mne.write_events(fname_out, sentnew2_events)
 
-# NEXT : loop through list_info for each sentence & add offset to timestamp
+# Loop through list_info for each sentence & add offset to timestamp
     num_sentences = (len(sentnew2_events)) - 1
 # Loop through sentences
-
     total_word_count = 0
     last_word = 0
     #for sentence_count in range(0, 3):
@@ -117,10 +115,6 @@ for i in subjs:
     fname_out_offset = op.join(data_path, '%s' %i, 'lists', 'ALL_%s-eve.lst'
                                %i)
     mne.write_events(fname_out_offset, b)
-
             
 #    fname_out_offset = op.join(data_path, '%s' %i, 'lists', 'ALL_%s-eve.lst' %i)
 #    mne.write_events(fname_out_offset, X)
-    
-
-
